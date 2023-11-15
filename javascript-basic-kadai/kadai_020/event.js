06a0682dfff2435d4eaafd24e3f12fac37ddfb6a
kokinta3155 <149220660+kokinta3155@users.noreply.github.com>
2023-11-15 23:28:15 +0900

    Add files via upload

---
+++ javascript-basic-kadai/kadai_020/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const body = document.getElementById('body');
+
+btn.addEventListener('click', ()=> {
+  
+  const text = document.getElementById('text');
+  text.textContent  = "ボタンをクリックしました"
+ 
+})
