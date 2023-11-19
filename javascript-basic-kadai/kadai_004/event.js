5e65b9cde7e3c16654180591682ff2c0c3f450eb
kokinta3155 <149220660+kokinta3155@users.noreply.github.com>
2023-11-19 19:22:13 +0900

    Add files via upload

---
+++ javascript-basic-kadai/kadai_004/event.js
@@ -0,0 +1,13 @@
+
+    // HTMLがロードされた場合
+    $(window).on({
+      'load':() =>{
+      console.log('loadイベントが発生しました');
+      }}),
+
+     // 画面がスクロールされた場合
+     $(window).on({
+      'scroll':() =>{
+      console.log('scrollイベントが発生しました');
+       }});
+ 
