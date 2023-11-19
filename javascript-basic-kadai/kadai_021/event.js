c1886c68a176e26f04812f0fe3374fec791ea7b9
kokinta3155 <149220660+kokinta3155@users.noreply.github.com>
2023-11-19 18:34:57 +0900

    Add files via upload

---
+++ javascript-basic-kadai/kadai_021/event.js
@@ -0,0 +1,6 @@
+//btn.addEventListener('click', function(){
+setTimeout(function textContent(){
+  //TODO: 下記を2秒経ってから実行する(ここで2秒待つ)
+  document.getElementById('text').textContent ='ボタンをクリックしました';
+}, 2000);
+//});
